529608b95c33c9a1fd9b823a90f61cc3bdee6253
Ondrej Zelina <ondrej.web@gmail.com>
2026-07-29 21:10:33 +0200

    Add deployment cert-manager

---
+++ helm/cert-manager/install.sh
@@ -0,0 +1,10 @@
+curl -LO https://cert-manager.io/public-keys/cert-manager-keyring-2021-09-20-1020CF3C033D4F35BAE1C19E1226061C665DF13E.gpg
+
+helm install \
+  cert-manager oci://quay.io/jetstack/charts/cert-manager \
+  --version v1.21.0 \
+  --namespace cert-manager \
+  --create-namespace \
+  --verify \
+  --keyring ./cert-manager-keyring-2021-09-20-1020CF3C033D4F35BAE1C19E1226061C665DF13E.gpg \
+  --set crds.enabled=true
